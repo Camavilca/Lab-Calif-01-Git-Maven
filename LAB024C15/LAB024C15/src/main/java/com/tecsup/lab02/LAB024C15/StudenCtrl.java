53fbd1c4ac07c6e4b7fda30ebd95233a9e97c28f
CAMAVILCA <orlando.camavilca@tecsup.edu.pe>
2018-09-06 14:25:36 -0500

    COMMIT SEXTO CAMAVILCA

--- LAB024C15/LAB024C15/src/main/java/com/tecsup/lab02/LAB024C15/StudenCtrl.java
+++ LAB024C15/LAB024C15/src/main/java/com/tecsup/lab02/LAB024C15/StudenCtrl.java
@@ -4,7 +4,19 @@ package com.tecsup.lab02.LAB024C15;
 public class StudenCtrl {
 	int numero1 = 5;
 	int numero2 = 4;
-	int resultado = numero1+numero2;
-
+	
+	/*
+	 * 
+	 * LOS CAMBIOS FUERON REALIZADOS POR EL RESPONSABLE
+	 * CAMBIOS RELIZADOS FUERON 
+	 * SUMA -MULTIPLICACION-RESTA-DIVICION
+	 */
+	int suma = numero1+numero2;
+    int multiplicacion  = numero1*numero2;
+    int resta  = numero1-numero2;
+    int divicion  = numero1/numero2;
+    String cambio  ="ESTE CAMBIO FUE REALIZADO POR ORLANDO"
+    		+ "CAMAVILCA CHAVEZ";
+    
 	
 }
